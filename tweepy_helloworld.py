d0b182511abe6a16b4b5499837f7751efa4be56a
Alex Warnes <warnes.alexander@gmail.com>
2017-02-01 15:26:48 -0800

    Update to pull random trend from top USA trends and get top 10 most popular tweets.

--- tweepy_helloworld.py
+++ tweepy_helloworld.py
@@ -5,6 +5,8 @@ Testing Tweepy API functions
 
 from private import secrets
 import tweepy
+from random import randint
+
 
 auth = tweepy.OAuthHandler(secrets.CONSUMER_KEY, secrets.CONSUMER_SECRET)
 
@@ -12,6 +14,21 @@ auth.set_access_token(secrets.ACCESS_TOKEN, secrets.ACCESS_SECRET)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
+usa_woeid = '23424977'
+
+usa_trends = api.trends_place(usa_woeid)
+
+# for trend in usa_trends[0]['trends']:
+#     print(trend['name'])
+
+random_trend = randint(0, len(usa_trends[0]['trends']))
+
+search_trend = usa_trends[0]['trends'][random_trend]['name']
+
+print(search_trend)
+
+top_10_tweets_from_search_trend = api.search(search_trend, count=10, result_type='popular', lang='en')
+
+for tweet in top_10_tweets_from_search_trend:
     print(tweet.text)
+    print('-'*len(tweet.text))
